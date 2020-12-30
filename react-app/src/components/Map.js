7dff538e5ab4c84d6022f7b2903b07fee2b13e8b
Rhys Previte <previterp88@gmail.com>
2020-12-29 19:55:47 -0500

    Adding source component with MapBox

--- react-app/src/components/Map.js
+++ react-app/src/components/Map.js
@@ -1,57 +1,65 @@
-import React, {useState} from "react"; 
-import ReactMapGL, {Marker, Layer} from "react-map-gl"; 
+import React, {useState, useEffect} from "react"; 
+import ReactMapGL, {Marker, Layer, Source} from "react-map-gl"; 
 import Pin from './Pin'; 
 import * as parkData from "./skateboard-parks.json"; 
 const mbxDirections = require('@mapbox/mapbox-sdk/services/directions');
 const directionsClient = mbxDirections({accessToken: "pk.eyJ1Ijoicmh5c3A4OCIsImEiOiJja2o5Yjc2M3kyY21iMnhwZGc2YXVudHVpIn0.c6TOaQ-C4NsdK9uZJABS_g"})
 
-//api request to the mapbox directions with SDK JS 
-let routeLayer; 
-
-directionsClient.getDirections({
-    profile: 'walking',
-    geometries: 'geojson', 
-    waypoints: [
-        {
-            coordinates: [-75.6972, 45.4215],
-            approach: 'unrestricted'
-        },
-        {
-            coordinates: [-75.546518086577947, 45.467134581917357]
-        }
-    ]
-})
-    .send()
-    .then(response => {
-        const route = response.body.routes[0].geometry.coordinates; 
-        const geojson = {
-            type: 'Feature', 
-            properties: {}, 
-            geometry: {
-                type: 'LineString', 
-                coordinates: route,
-            }
-        };
-        //assume no route currently exists (at this point)
-        routeLayer = {
-            id: 'route',
-            type: 'line', 
-            source: {
-                type: 'geojson',
-                data: {
-                    type: 'Feature', 
-                    properties:{}, 
-                    geometry: {
-                        type: 'LineString', 
-                        coordinates: geojson,
-                    }
-                }
-            }
-        }    
-    });
 
 
 const Map = () => {
+
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [data, setData] = useState({})
+    
+    useEffect(() => {
+    //api request to the mapbox directions with SDK JS 
+    
+    directionsClient.getDirections({
+        profile: 'walking',
+        geometries: 'geojson', 
+        waypoints: [
+            {
+                coordinates: [-75.6972, 45.4215],
+                approach: 'unrestricted'
+            },
+            {
+                coordinates: [-75.546518086577947, 45.467134581917357]
+            }
+        ]
+    })
+        .send()
+        .then(response => {
+            const route = response.body.routes[0].geometry.coordinates; 
+            const geojson = {
+                type: 'FeatureCollection', 
+                properties: {}, 
+                geometry: {
+                    type: 'LineString', 
+                    coordinates: route,
+                }
+            };
+            //assume no route currently exists (at this point)
+            setRouteLayer({
+                id: 'route',
+                type: 'line', 
+                source: {
+                    type: 'geojson',
+                    data: {
+                        type: 'Feature', 
+                        properties: {}, 
+                        geometry: {
+                            type: 'LineString', 
+                            coordinates: geojson,
+                        }
+                    }
+                }
+            })
+            setIsLoaded(true);
+        });
+    }, []);
+
+
     //set viewport
     const [viewport, setViewport] = useState({
         latitude: 45.4211, 
@@ -89,11 +97,8 @@ const Map = () => {
             longitude: event.lngLat[0],
         })
     } 
-    
-    //set layer 
-    
 
-    return (
+    return isLoaded && (
         <ReactMapGL {...viewport} 
         mapboxApiAccessToken={"pk.eyJ1Ijoicmh5c3A4OCIsImEiOiJja2o5Yjc2M3kyY21iMnhwZGc2YXVudHVpIn0.c6TOaQ-C4NsdK9uZJABS_g"}
         mapStyle={"mapbox://styles/rhysp88/ckj950pju3y8l1aqhpb58my9d/draft"}
@@ -102,7 +107,7 @@ const Map = () => {
             onDragEnd={dragHandlerEnd} draggable={true}>
                     <Pin />
             </Marker>
-        
+            <Layer {...routeLayer} paint={{'line-color': '#3887be', 'line-width': 5, 'line-opacity': 0.75}} />
             {/* {parkData.features.map(data => {
                let longitude = data.geometry.coordinates[0];
                let latitude = data.geometry.coordinates[1];
