ff503367c556b4901ac49efa392e6361828fdc94
Kyle Barthelmes <kbart2401@gmail.com>
2020-12-30 16:32:14 -0500

    restore user route returning created running routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.rival_routes import rival_routes
+from .api.route_routes import route_routes
 
 from .seeds import seed_commands
 
@@ -33,6 +34,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(rival_routes, url_prefix='/api/rivals')
+app.register_blueprint(route_routes, url_prefix='/api/routes')
 db.init_app(app)
 Migrate(app, db)
 
--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User, Route, RunTime, db
+from app.models import User, Route, RunTime, db, rivals_table
+from sqlalchemy.orm import aliased
 
 user_routes = Blueprint('users', __name__)
 
@@ -18,6 +19,23 @@ def user(id):
     user = User.query.get(id)
     # get total running time
     total_times = RunTime.query.filter_by(user_id=user.id).all()
+    # get user created routes
+    data = Route.query.filter_by(user_creator=user.id).all()
+    def route_to_dict(obj):
+        return {"name": obj.name, "user_creator": obj.user_creator, 
+        "route_coordinates": obj.route_coordinates, "date_created": obj.date_created}
+    created_routes = map(route_to_dict, data)
+    my_routes = tuple(created_routes)
+    # get user rivals
+    rivals = user.rivals
+    if rivals:
+        def to_obj(obj):
+            return {'first_name': obj.first_name, "last_name": obj.last_name, 
+                "email": obj.email, "gender": obj.gender}
+        new_rivals = map(to_obj, rivals)
+        my_rivals = tuple(new_rivals)
+    else:
+        my_rivals = ''
     if total_times:
         if isinstance(total_times, list):
             def run_times(total_time):
@@ -26,8 +44,9 @@ def user(id):
             user_total_run_time = sum(user_times)
         else:
             user_total_run_time = total_times.time
-        return {'user': user.to_dict(), "total_time": user_total_run_time}
-    return {'user': user.to_dict()}
+        return {'user': user.to_dict(), "total_time": user_total_run_time, 
+                "rivals": my_rivals, "created_routes": my_routes}
+    return {'user': user.to_dict(), "rivals": my_rivals, "created_routes": created_routes.to_dict()}
 
 
 # Add running route
