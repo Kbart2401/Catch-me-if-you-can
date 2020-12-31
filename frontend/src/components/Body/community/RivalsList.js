fb6a92d0a52049f340ba5806f8f1ed558e9289d8
Nick Richard <nrich414@gmail.com>
2020-12-30 19:13:33 -0500

    loval changes to RiavalsList

--- frontend/src/components/Body/community/RivalsList.js
+++ frontend/src/components/Body/community/RivalsList.js
@@ -1,27 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import * as sessionActions from "/home/nick/app_academy/Week_20_group_project2/CatchMeIfYouCan/frontend/src/store/actions/session.js";
+import { useDispatch } from "react-redux";
 
 function RivalsList() {
-  const loadedRivals = useSelector(state => state.session.rivals)
-  const user = useSelector(state => state.session.user)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [rivals, setRivals] = useState([])
+	const dispatch = useDispatch();
+	const loadedRivals = useSelector((state) => state.session.rivals);
+	const loadedUser = useSelector((state) => state.session.user);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [rivals, setRivals] = useState([]);
+	const [user, setUser] = useState({});
 
-  useEffect(() => {
-    setIsLoaded(true)
-  }, []);
+	useEffect(() => {
+		setUser(loadedUser)(dispatch(sessionActions.retrieveRivals(user.id)))
+			.then((data) => setRivals(data))
+			.then(setIsLoaded(true));
+	}, []);
 
-  return isLoaded && (
-    <>
-      <h1>Rival List: </h1>
-      <ul>{rivals.map(rival => (
-        <li key={rival.id}>
-          <NavLink to={`/users/${rival.id}`}>{rival.username}</NavLink>
-        </li>
-      ))}</ul>
-    </>
-  );
+	return (
+		isLoaded && (
+			<>
+				<h1>Rival List: </h1>
+				<ul>
+					{rivals.map((rival) => (
+						<li key={rival.id}>
+							<NavLink to={`/users/${rival.id}`}>{rival.first_name}</NavLink>
+						</li>
+					))}
+				</ul>
+			</>
+		)
+	);
 }
 
 export default RivalsList;
