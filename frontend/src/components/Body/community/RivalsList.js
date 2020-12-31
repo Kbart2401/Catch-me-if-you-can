adb706ab9a1c65208cdce293dbea5e657606e642
Nick Richard <nrich414@gmail.com>
2020-12-30 19:47:29 -0500

    fixed rivals list (async)

--- frontend/src/components/Body/community/RivalsList.js
+++ frontend/src/components/Body/community/RivalsList.js
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import * as sessionActions from "/home/nick/app_academy/Week_20_group_project2/CatchMeIfYouCan/frontend/src/store/actions/session.js";
+import * as sessionActions from "../../../store/actions/session.js";
 import { useDispatch } from "react-redux";
 
 function RivalsList() {
 	const dispatch = useDispatch();
 	const loadedRivals = useSelector((state) => state.session.rivals);
-	const loadedUser = useSelector((state) => state.session.user);
+	const user = useSelector((state) => state.session.user);
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [rivals, setRivals] = useState([]);
-	const [user, setUser] = useState({});
-
 	useEffect(() => {
-		setUser(loadedUser)(dispatch(sessionActions.retrieveRivals(user.id)))
-			.then((data) => setRivals(data))
-			.then(setIsLoaded(true));
-	}, []);
+		if (user) {
+			dispatch(sessionActions.retrieveRivals(user.id))
+				.then((data) => setRivals(data.rivals))
+				.then(setIsLoaded(true));
+		}
+	}, [user]);
 
 	return (
 		isLoaded && (
