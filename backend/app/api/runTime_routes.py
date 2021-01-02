8ead93e11b01ca35b220721aaec3f8b0c1e5237f
Kyle Barthelmes <kbart2401@gmail.com>
2021-01-02 17:33:15 -0500

    added return all created routes route

--- backend/app/api/runTime_routes.py
+++ backend/app/api/runTime_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import User, Route, RunTime, db
 
 runTime_routes = Blueprint('runtimes', __name__)
@@ -8,14 +8,20 @@ runTime_routes = Blueprint('runtimes', __name__)
 @runTime_routes.route('/', methods=['POST'])
 @login_required
 def add_runtime():
-  user_id = request.get_json('id')
-  route_id = request.get_json('routeId')
-  time = request.get_json('time')
-  run_time = RunTime(
-    user_id = user_id,
-    route_id = route_id,
-    time = time
-  )
-  db.session.add(run_time)
-  db.session.commit()
-  return run_time.to_dict()
+    user_id = request.get_json().get('id')
+    route_id = request.get_json().get('routeId')
+    time = request.get_json().get('time')
+    run_time = RunTime(
+        user_id=user_id,
+        route_id=route_id,
+        time=time
+    )
+    db.session.add(run_time)
+    db.session.commit()
+    return run_time.to_dict()
+
+# Search for all runTimes associated with specific route
+@runTime_routes.route('/<int:route_id>')
+@login_required
+def get_runtimes_for_route(route_id):
+    
