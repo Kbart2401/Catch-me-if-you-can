46d168484c5ecedad532d1bfa177e1eae92357e1
Rhys Previte <previterp88@gmail.com>
2021-01-01 21:01:08 -0500

    Added turf library and changed marker functionality for search map

--- frontend/src/components/Body/MapSearch.js
+++ frontend/src/components/Body/MapSearch.js
@@ -4,11 +4,11 @@ import Pin from './Pin';
 import { useSelector } from 'react-redux';
 import './Map.css';
 const mbxDirections = require('@mapbox/mapbox-sdk/services/directions');
-const directionsClient = mbxDirections({ accessToken: "pk.eyJ1Ijoicmh5c3A4OCIsImEiOiJja2pjMDUzYnozMzVhMzBucDAzcXBhdXdjIn0.kEXpfO6zDjp9J4QXnwzVcA" })
+import * as turf from '@turf/turf'
 
 const MapSearch = () => {
   const [viewport, setViewport] = useState({});
-  const [markers, setMarkers] = useState([])
+  const [marker, setMarker] = useState([])
   const [routeData, setRouteData] = useState({})
   const [isLoaded, setIsLoaded] = useState(false)
   const [distance, setDistance] = useState(0)
@@ -35,85 +35,44 @@ const MapSearch = () => {
   function error(err) {
     alert(`ERROR(${err.code}): ${err.message}`);
   };
+  
+  navigator.geolocation.getCurrentPosition(success, error);
+  
+  //click event for dropping marker on map
+  function clickMarker(event) {
+    setMarker([event.lngLat[1], event.lngLat[0]])
+    console.log(marker); 
+  };
 
   //Get geolocation and set marker locations
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success, error);
-    // async function getMarkers() {
-    //   const res = await fetch('/api/routes')
-    //   const data = await res.json()
-    //   setMarkers(data)
-    // }
-    if (createdRoutes) {
-      const routeMarkers = createdRoutes.map(route => {
-        return route.route_coordinates[0]
-      })
-      setMarkers(routeMarkers)
-      }
-}, [createdRoutes])
-
-
-  //api request to the mapbox directions with SDK JS 
-  useEffect(() => {
-    if (markers.length >= 2) {
-      const ways = markers.map(marker => {
-        return {
-          coordinates: [marker[1], marker[0]]
-        }
-      });
-      directionsClient.getDirections({
-        profile: 'walking',
-        geometries: 'geojson',
-        waypoints: ways
-      })
-        .send()
-        .then(response => {
-          console.log(response.body)
-          const route = response.body.routes[0].geometry.coordinates;
-          const dist = response.body.routes[0].distance;
-
-          const waypoints = response.body.waypoints;
-          let nameArr = [];
-          for (let i = 0; i < waypoints.length; i++) {
-            let name = waypoints[i].name;
-            nameArr.push(name);
-          }
-
-          const geojson = {
-            type: 'FeatureCollection',
-            features: [
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'LineString',
-                  coordinates: route,
-                },
-              }
-            ]
-          };
-          setName(nameArr);
-          setDistance(dist);
-          setRouteData({ ...geojson });
-          setIsLoaded(true);
-        });
-    }
-  }, [markers]);
-
+  // useEffect(() => {
+  //   // async function getmarker() {
+  //   //   const res = await fetch('/api/routes')
+  //   //   const data = await res.json()
+  //   //   setMarker(data)
+  //   // }
+  //   if (createdRoutes) {
+  //     const routemarker = createdRoutes.map(route => {
+  //       return route.route_coordinates[0]
+  //     })
+  //     setMarker(routemarker)
+  //     }
+  // }, [createdRoutes])
 
 return (
   <div className={"map_container"}>
     <ReactMapGL {...viewport}
       mapboxApiAccessToken={"pk.eyJ1Ijoicmh5c3A4OCIsImEiOiJja2o5Yjc2M3kyY21iMnhwZGc2YXVudHVpIn0.c6TOaQ-C4NsdK9uZJABS_g"}
       mapStyle={"mapbox://styles/rhysp88/ckj950pju3y8l1aqhpb58my9d/draft"}
-      onViewportChange={viewport => setViewport(viewport)}>
-      {markers.length === 1 &&
-        <Marker latitude={markers[0][0]} longitude={markers[0][1]}>
+      onViewportChange={viewport => setViewport(viewport)} onClick={clickMarker}>
+      {marker.length === 2 && 
+        <Marker latitude={marker[0]} longitude={marker[1]}>
           <Pin />
         </Marker>
-      }
-      {isLoaded &&
+      }      
+      {/* {isLoaded &&
         <>
-          {markers.map((marker, i) => {
+          {marker.map((marker, i) => {
             return (
               <Marker latitude={marker[0]} longitude={marker[1]}>
                 <button
@@ -145,7 +104,7 @@ return (
             </Popup>
           ) : null}
         </>
-      }
+      } */}
     </ReactMapGL>
   </div>
 )
