4a99f67a1ec9a3c55076028e8d04f7f7dbfcc1d3
Rhys Previte <previterp88@gmail.com>
2021-01-03 17:34:17 -0500

    Adding CSS to create routes

--- frontend/src/components/Body/RoutesPage/Pin.js
+++ frontend/src/components/Body/RoutesPage/Pin.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Pin = () => {
     return (
-        <span>
-            <i className="fa fa-play" aria-hidden="true"></i>
-        </span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="5" cy="10" r="5" z-index="1"></circle>
+        </svg>
     )
 }
 
