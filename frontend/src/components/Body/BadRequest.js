2321f5160a4fd25f80d0149b87dab3749d17ba3d
Kyle Barthelmes <kbart2401@gmail.com>
2021-02-05 14:07:52 -0500

    working on 404 page

---
+++ frontend/src/components/Body/BadRequest.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BadRequest = () => {
+
+
+  return (
+    <>
+    <div className='bad-request-header'>404</div>
+      <div>Lost in the travel of time and space are you? Here's a fellow sojourner to keep you company</div>
+      <img src='/pictures/giphy.gif' />
+    </>
+  )
+}
+
+export default BadRequest
