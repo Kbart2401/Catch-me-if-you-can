6978b653e65db8b51983868d1767b327e62f96ee
Kyle Barthelmes <kbart2401@gmail.com>
2021-02-05 20:00:51 -0500

    created 404 page

--- frontend/src/components/Body/BadRequest.js
+++ frontend/src/components/Body/BadRequest.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 
 const BadRequest = () => {
+  const header1 = useRef(null)
+  const header2 = useRef(null)
+  const text1 = useRef(null)
+  const text2 = useRef(null)
+  const image = useRef(null)
 
+  useEffect(() => {
+      header1.current.classList.remove('begin')
+      header2.current.classList.remove('begin')
+      text1.current.classList.remove('begin')
+      text2.current.classList.remove('begin')
+      image.current.classList.remove('begin')
+  }, [])
 
   return (
     <>
-    <div className='bad-request-header'>404</div>
-      <div>Lost in the travel of time and space are you? Here's a fellow sojourner to keep you company</div>
-      <img src='/pictures/giphy.gif' />
+      <div className='bad-request-container'>
+        <video autoPlay muted src='https://cdn.videvo.net/videvo_files/video/free/2013-10/small_watermarked/Background_08_preview.webm' 
+        type='video/webm' loop />
+        <div className='bad-request-header header1 begin' ref={header1}>404</div>
+        <div className='bad-request-header header2 begin' ref={header2}>Page Not Found</div>
+        <div className='bad-request-text text1 begin' ref={text1}>Lost in the travel of time and space are you?</div>
+        <div className='bad-request-text text2 begin' ref={text2}> Here's a fellow sojourner to keep you company</div>
+        <br></br>
+        <img className='bad-request-img begin' src='/pictures/giphy.gif' ref={image}/>
+      </div>
     </>
   )
 }
